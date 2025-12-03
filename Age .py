0151d65fdb37c7fc0ca343aecc0576cb3f8097d2
sashabeck-star <sasha.beck.2025@gmail.com>
2025-12-02 18:47:02 -0600

    Update Age .py

--- Age .py
+++ Age .py
@@ -1,5 +1,12 @@
-#Program Description This program eavlutes age and drive 
+#Program Description: This program checks the user age and confirms their driving eligiblity
 
-#Getting the age from the user 
 
-test_score = int(input("Entre your  ))
+#Variable to store age of user and recieve input
+age = int(input("Enter your age: "))
+
+#if else statements to evaluate if user can drive
+
+if(age >= 16):
+    print("Your age is eligible to drive. ")
+else:
+    print("Your age is ineligible to drive. ")
